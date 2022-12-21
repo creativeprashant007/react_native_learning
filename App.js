293959f918e3dbd05ae5db791137cecb11480c9c
creativeprashant007 <prashanttimalsina18@gmail.com>
2022-12-21 15:01:54 +0545

    React Native Style sheet

--- App.js
+++ App.js
@@ -1,47 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, SafeAreaView, Image } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  Platform,
+} from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("I press on text widget");
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text} numberOfLines={2} onPress={handlePress}>
-        Hello this is testing
-      </Text>
-      <Button
-        onPress={() => {
-          alert("You tabbed the button");
-        }}
-        title="Press me"
-      />
-      <Image
-        blurRadius={2}
-        resizeMode={"contain"}
-        source={{
-          height: 200,
-          width: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
-      <StatusBar style="auto" />
+    <SafeAreaView style={(styles.container, safeAreaStyle)}>
+      <Button style={{ backgroundColor: "#FFF" }} title="Press me" />
     </SafeAreaView>
   );
 }
 
+const safeAreaStyle = { backgroundColor: "#000" };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FF0",
+    backgroundColor: "orange",
     alignItems: "center",
     justifyContent: "center",
-  },
-  text: {
-    fontSize: 39,
-    color: "#000",
-  },
-  image: {
-    height: 100,
-    width: 100,
+    //marginTop: Platform.OS === "ios" ? 0 : 0,
   },
 });
