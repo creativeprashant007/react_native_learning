743ad861b7b3a17e27c40e765ba1f6c1901e1fb3
creativeprashant007 <prashanttimalsina18@gmail.com>
2022-12-29 17:11:53 +0545

    feat:icon added

--- app/screens/todo/ToDoMainScreen.js
+++ app/screens/todo/ToDoMainScreen.js
@@ -81,9 +81,10 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 40,
-    flex: 1,
+    // flex: 1,
   },
   list: {
+    //flex: 1,
     marginTop: 20,
   },
 });
--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { Text, StyleSheet, TouchableOpacity } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
-
+import { Text, StyleSheet, TouchableOpacity, View } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 function ToDoItem({ item, pressHandler }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.itemStyle}>{item.text}</Text>
-      <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      <View style={styles.itemStyle}>
+        <Text>{item.text}</Text>
+        <Entypo style={styles.iconStyle} name="cross" size={24} color="black" />
+        {/* <Ionicons
+          
+          name="md-delete"
+          size={32}
+          color="green"
+        /> */}
+      </View>
     </TouchableOpacity>
   );
 }
 const styles = StyleSheet.create({
   itemStyle: {
+    flex: 1,
     padding: 16,
     marginTop: 16,
     borderColor: "#bbb",
@@ -19,6 +27,13 @@ const styles = StyleSheet.create({
     borderRadius: 1,
     borderStyle: "dotted",
     borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  toDoTtem: {},
+  iconStyle: {
+    // paddingHorizontal: 20,
   },
 });
 export default ToDoItem;
