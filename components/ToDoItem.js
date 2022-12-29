1dd4e65f7c6378853398c35fd5aae6fcc2a71f46
creativeprashant007 <prashanttimalsina18@gmail.com>
2022-12-29 16:53:55 +0545

    feat:icons

--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Text, StyleSheet, TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 function ToDoItem({ item, pressHandler }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
       <Text style={styles.itemStyle}>{item.text}</Text>
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />
     </TouchableOpacity>
   );
 }
