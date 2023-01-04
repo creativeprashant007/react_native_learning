18ea6945593b7d1eea99829c12290bdfdec8ec90
creativeprashant007 <prashanttimalsina18@gmail.com>
2023-01-04 18:11:46 +0545

    fix:navigation

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 import Home from "./app/screens/reviewapp/Home";
-import { AppLoading } from "expo";
+// import { AppLoading } from "expo";
 import * as Font from "expo-font";
-import { useState } from "react";
-import Navigator from "./routes/HomeStack";
-
-const getFonts = () =>
-  Font.loadAsync({
-    "nunito-regular": require("./app/assets/fonts/Nunito-Regular.ttf"),
-    "nunito-bold": require("./app/assets/fonts/Nunito-Bold.ttf"),
-  });
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// const getFonts = () =>
+//   Font.loadAsync({
+//     "nunito-regular": require("./app/assets/fonts/Nunito-Regular.ttf"),
+//     "nunito-bold": require("./app/assets/fonts/Nunito-Bold.ttf"),
+//   });
+const Stack = createNativeStackNavigator();
 export default function App() {
-  // const [fontsLoaded, setFontsLoaded] = useState(false);
-  // if (fontsLoaded) {
-  return <Navigator />;
-  //  } else {
-  // return (
-  //   <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
-  // );
-  //}
+ 
+  return (<NavigationContainer>
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={Home} />
+  </Stack.Navigator>
+</NavigationContainer>);
+
 }
