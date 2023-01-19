ec83a74b2541e0008ba1bb0923aa1ff8e10c488e
creativeprashant007 <prashanttimalsina18@gmail.com>
2023-01-19 17:54:04 +0545

    fix:image path

--- app/screens/reviewapp/ReviewDetails.js
+++ app/screens/reviewapp/ReviewDetails.js
@@ -5,15 +5,22 @@ import AppCard from "../../../shared/card";
 import { globalStyles } from "../../../styles/global_style";
 function ReviewDetails({ route, navigation }) {
   const { title, reating, body, key } = route.params;
-var url = '../../assets/rating-' + reating + '.png'
+const imgSrc = {
+  "1":require("../../assets/rating-1.png"),
+  "2":require("../../assets/rating-2.png"),
+  "3":require("../../assets/rating-3.png"),
+  "4":require("../../assets/rating-4.png"),
+  "5":require("../../assets/rating-5.png")
+  };
+
   return (
     <View style={globalStyles.container}>
       <AppCard>
         <Text style={globalStyles.titleText}>{title}</Text>
-        <Text style={globalStyles.titleText}>{body}{reating}</Text>
+        <Text style={globalStyles.titleText}>{body}</Text>
         <View style ={styles.rating}>
           <Text>GameZone Rating:</Text>
-          <Image source = {require(url)} />
+          <Image source = {imgSrc[reating]} />
 
         </View>
       </AppCard>
